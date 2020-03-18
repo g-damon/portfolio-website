65231373df83f767be24b90520440304632f4bfc
Bone Naked <damnmulletdesign@gmail.com>
2020-03-18 00:51:07 -0700

    h

--- MySite/assets/js/main.js
+++ MySite/assets/js/main.js
@@ -142,12 +142,4 @@
 
 			});
 
-			$(function () {
-				const parent = $("#shuffle");
-				var divs = parent.children();
-				while (divs.length) {
-					parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-				}
-			});
-
 })(jQuery);
